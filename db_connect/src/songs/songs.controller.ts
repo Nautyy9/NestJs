103fe16cc7643b8c17bd5d009d4249eca6f1dc36
NITIN NAUTIYAL <nitinnautiyal2016@gmail.com>
2024-10-14 05:51:20 +0530

    added Readme

---
+++ db_connect/src/songs/songs.controller.ts
@@ -0,0 +1,94 @@
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  Scope,
+} from '@nestjs/common';
+import { SongsService } from './songs.service';
+
+import { Connection } from 'types/types';
+import { Song } from './song.entity';
+import { CreateSongDTO } from './songs_dto/songs.dto';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { UpdateSongDto } from './songs_dto/update_song.dto';
+
+@Controller('songs')
+export class SongsController {
+  constructor(
+    private songsService: SongsService,
+    @Inject('CONNECTION')
+    private connection: Connection,
+  ) {
+    console.log(` Connection object ${this.connection.url}`);
+  }
+
+  @Get()
+  findAll(
+    @Query("page" , new DefaultValuePipe(1) , ParseIntPipe)
+    page = 1,
+    @Query("limit" , new DefaultValuePipe(10) , ParseIntPipe)
+    limit
+  ): Promise<Song[]> {
+    try {
+      return this.songsService.findAll();
+    } catch (e) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  @Post()
+  create(@Body() song: CreateSongDTO): Promise<Song> {
+    try {
+      return this.songsService.create(song);
+    } catch (e) {
+      throw new HttpException(
+        `Sever Error: `,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  @Get(':id')
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ): Promise<Song> {
+    return this.songsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateSongDTO: UpdateSongDto,
+  ): Promise<UpdateResult> {
+    return this.songsService.update(id, updateSongDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.songsService.remove(id);
+  }
+}
